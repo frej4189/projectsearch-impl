8621ee7e7622766afd2462614f1203f23031debe
Frej Alexander Nielsen <frej@valoks.com>
2024-02-24 10:21:15 +0100

    Initial products service implementation

--- src/app/services/products.service.spec.ts
+++ src/app/services/products.service.spec.ts
@@ -1,16 +1,28 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ProductsService } from './products.service';
+import { HttpClientModule } from '@angular/common/http';
+import { AppModule } from '../app.module';
 
 describe('ProductsService', () => {
   let service: ProductsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        AppModule
+      ]
+    });
     service = TestBed.inject(ProductsService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should return a list of products', waitForAsync(() => {
+    service.getProductList().subscribe(products => {
+      expect(products?.length ?? 0).toBeGreaterThan(0);
+    });
+  }));
 });
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { SearchProductResponse } from '../products';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getProductList() {
+    return this.http.get<SearchProductResponse>('assets/products.json').pipe(
+      map(response => response.content)
+    );
+  }
 }
