ff91ae4810542a0c3429a398035049d543582310
Frej Alexander Nielsen <frej@valoks.com>
2024-02-24 10:59:39 +0100

    Load products on init in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from './services/products.service';
+import { Product } from './products';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'projectsearch-impl';
+export class AppComponent implements OnInit {
+
+  products: BehaviorSubject<Product[]> = new BehaviorSubject<Product[]>([]);
+
+  constructor(private productsService: ProductsService) { }
+
+  ngOnInit() {
+    this.productsService.getProductList().subscribe(this.products);
+  }
+
 }
